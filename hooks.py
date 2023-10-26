7a3dd445fbf17a22416554aa7d119085adfc0d0d
byronlau <byronlau666@gmail.com>
2023-10-26 13:11:07 +0800

    init

--- hooks.py
+++ hooks.py
@@ -1,3 +1,3 @@
 from datetime import datetime
 def on_config(config, **kwargs):
-    config.copyright = f"版权所有 © 2023-{datetime.now().year} <a href=\"https://github.com/byronlauee/discussions\" target=\"_blank\">橘涂拾捌</a>"
+    config.copyright = f"版权所有 © 2023-{datetime.now().year} <a href=\"https://github.com/byronlau/Knowledge-Garden/discussions\" target=\"_blank\">橘涂拾捌</a>"
